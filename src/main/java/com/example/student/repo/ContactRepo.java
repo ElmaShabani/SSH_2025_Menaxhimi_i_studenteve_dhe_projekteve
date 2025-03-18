5012118d204d69b5502d04d9de5665b139ae2be7
EElmaShabani <elma.shabani1@student.uni-pr.edu>
2025-03-18 11:30:39 +0100

    Contact

---
+++ src/main/java/com/example/student/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package com.example.student.repo;
+
+import com.example.student.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
